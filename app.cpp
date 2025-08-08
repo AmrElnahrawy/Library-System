c4852b7070bce947c8783d95acc6ccf41783eefd
Amr Elnahrawy <amrelnahrawy05@gmail.com>
2025-08-08 20:29:21 +0300

    implement printing with id and name

--- app.cpp
+++ app.cpp
@@ -37,7 +37,6 @@ struct library_system
         return choice;
     }
     
-    
     void run()
     {    
         while (true)
@@ -49,10 +48,10 @@ struct library_system
                 search_book();
             // else if (choice == 3)
             //     print_borrowers();
-            // else if (choice == 4)
-            //     print_library_by_id();
-            // else if (choice == 5)
-            //     print_library_by_name();
+            else if (choice == 4)
+                print_library_by_id();
+            else if (choice == 5)
+                print_library_by_name();
             // else if (choice == 6)
             //     add_user();
             // else if (choice == 7)
@@ -68,7 +67,7 @@ struct library_system
 
     void add_book() {
         if (add == maxCap) {
-            cout << "Sorry the library if full";
+            cout << "Sorry the library is full";
             return;
         }
         int id , quantity;
@@ -76,7 +75,7 @@ struct library_system
         cout << "Enter book ID (integer) , Name , Quantity: ";
         cin >> id >> name >> quantity;
         
-        for (int i {0}; i <= add; i++) {
+        for (int i {0}; i < add; i++) {
             if (books[i].id == id) {
                 books[i].quantity += quantity;
                 return;
@@ -110,6 +109,39 @@ struct library_system
         }
         if (!(flag)) {
             cout << "There is no book with this prefix";
+        }
+        cout << "\n****************************************\n";
+    }
+
+    // void print_borrowers() {
+
+    // }
+
+    static bool id_sort(library_books &a, library_books &b) {
+        return a.id < b.id;
+    }
+    void print_library_by_id() {    
+        cout << "\n****************************************\n";
+        if (add == 0)
+            cout << "There are no books to be printed\n";
+        sort(books, books + add, id_sort);
+        for (int i {0}; i < add; i++) {
+            cout << "ID: " << books[i].id << ", Name: " << books[i].name << ", quantity: " << books[i].quantity << endl;
+        }
+        cout << "\n****************************************\n";
+    }
+
+    static bool name_sort(library_books &a, library_books &b) {
+        return a.name < b.name;
+    }
+    void print_library_by_name() {
+        
+        cout << "\n****************************************\n";
+        if (add == 0)
+            cout << "There are no books to be printed\n";
+        sort(books, books + add, name_sort);
+        for (int i {0}; i < add; i++) {
+            cout << "ID: " << books[i].id << ", Name: " << books[i].name << ", quantity: " << books[i].quantity << endl;
         }
         cout << "\n****************************************\n";
     }
