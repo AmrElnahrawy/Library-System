70caa7a877d8df8b51f1eb8772e0b1a7c8306716
Amr Elnahrawy <amrelnahrawy05@gmail.com>
2025-08-08 16:45:32 +0300

    implement add and search for books

--- app.cpp
+++ app.cpp
@@ -1,15 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+const int maxCap = 100;
 struct library_books
 {
-    
+    int id;
+    string name;
+    int quantity;
 };
 
 struct library_system
 {
+    library_books books[maxCap];
+    int add {0};
     int menu() {
-        int choice = -1;
+        int choice {-1};
         while (choice == -1) {
             cout << "Library Menu:" << endl;
             cout << "1) Add book" << endl;
@@ -24,7 +29,7 @@ struct library_system
             cout << "10) Exit" << endl << endl;
             cout << "Enter your menu choice [1 - 10]: ";
             cin >> choice;
-            if (1 <= choice && choice <= 10) {
+            if (!(1 <= choice && choice <= 10)) {
                 cout << "Invalid choice, Please try again" << endl;
                 choice = -1;   
             }
@@ -39,50 +44,79 @@ struct library_system
         {
             int choice = menu();
             if (choice == 1)
-            {
                 add_book();
-            }
             else if (choice == 2)
-            {
                 search_book();
-            }
-            else if (choice == 3)
-            {
-                print_borrowers();
-            }
-            else if (choice == 4)
-            {
-                print_library_by_id();
-            }
-            else if (choice == 5)
-            {
-                print_library_by_name();
-            }
-            else if (choice == 6)
-            {
-                add_user();
-            }
-            else if (choice == 7)
-            {
-                user_borrow_book();
-            }
-            else if (choice == 8)
-            {
-                user_return_book();
-            }
-            else if (choice == 9)
-            {
-                print_users();
-            }
-            else if (choice == 10)
-            {
+            // else if (choice == 3)
+            //     print_borrowers();
+            // else if (choice == 4)
+            //     print_library_by_id();
+            // else if (choice == 5)
+            //     print_library_by_name();
+            // else if (choice == 6)
+            //     add_user();
+            // else if (choice == 7)
+            //     user_borrow_book();
+            // else if (choice == 8)
+            //     user_return_book();
+            // else if (choice == 9)
+            //     print_users();
+            // else
+            //     return;
+        }
+    }
+
+    void add_book() {
+        if (add == maxCap) {
+            cout << "Sorry the library if full";
+            return;
+        }
+        int id , quantity;
+        string name;
+        cout << "Enter book ID (integer) , Name , Quantity: ";
+        cin >> id >> name >> quantity;
+        
+        for (int i {0}; i <= add; i++) {
+            if (books[i].id == id) {
+                books[i].quantity += quantity;
                 return;
             }
         }
+        books[add].id = id;
+        books[add].name = name;
+        books[add].quantity = quantity;
+        add++;
     }
-}
+
+    void search_book() {
+        string prefix;
+        cout << "Please enter the prefix of the book: ";
+        cin >> prefix;
+        cout << "****************************************\n";
+        int flag = 0;
+        for (int i {0}; i < add; i++) {
+            for (int j {0}; j < prefix.size(); j++) {
+                if (prefix[j] != books[i].name[j])
+                    break;
+                if (j == prefix.size() - 1) {
+                    if (flag) {
+                        cout << ", ";
+                        flag = 0;
+                    }
+                    cout << books[i].name;
+                    flag = 1;
+                }
+            }
+        }
+        if (!(flag)) {
+            cout << "There is no book with this prefix";
+        }
+        cout << "\n****************************************\n";
+    }
+};
 
 int main()
 {
-
+    library_system library;
+    library.run();
 }
